8a47545398ab807089da341afeea295c0b59838f
naryyyyy <133038281+naryyyyy@users.noreply.github.com>
2023-06-23 21:17:25 +0300

    Add files via upload

---
+++ lesson1/main.js
@@ -0,0 +1,43 @@
+const som=document.querySelector('#som')
+const usd=document.querySelector('#usd')
+const eur=document.querySelector('#eur')
+//som.addEventListener('input',()=>{
+//   const request=new XMLHttpRequest()
+//   request.open=("GET", "data.json")
+//   request.setRequestHeader("Content-type", "application/json")
+//   request.send()
+//   request.onload=()=>{
+//      const response=JSON.parse(request.response)
+//      usd.value=(som.value/response.usd).toFixed(2)
+//   }
+//})
+const convert = (currency, targetInput1, targetInput2, isTrue, currencyType) => {
+   currency.oninput = () => {
+       const request = new XMLHttpRequest()
+       request.open("GET", "data.json")
+       request.setRequestHeader("Content-type", "application/json")
+       request.send()
+       request.onload = () => {
+           const response = JSON.parse(request.response)
+           if (isTrue && currencyType === "usd") {
+               targetInput1.value = (currency.value / response.usd).toFixed(2)
+               targetInput2.value = (currency.value / response.eur).toFixed(2)
+           } else if (isTrue && currencyType === "eur") {
+               targetInput1.value = (currency.value * response.eur).toFixed(2)
+               targetInput2.value = (currency.value * response.eur).toFixed(2)
+           } else if (!isTrue && currencyType === "usd") {
+               targetInput1.value = (currency.value * response.usd).toFixed(2)
+               targetInput2.value = (currency.value * (response.usd/response.eur)).toFixed(2)
+           } else if (!isTrue && currencyType === "eur") {
+               targetInput1.value = (currency.value ( response.eur/response.usd)).toFixed(2)
+               targetInput2.value = (currency.value * response.eur).toFixed(2)
+           }
+           currency.value === '' && (targetInput1.value = '')
+           currency.value === '' && (targetInput2.value = '')
+       }
+   }
+}
+
+convert(som, usd, eur, true, "usd")
+convert(usd, som, eur, false, "usd")
+convert(eur, som, usd, false, "eur")
